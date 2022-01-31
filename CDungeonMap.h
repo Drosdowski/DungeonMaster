912c3ed4f0d060a698b6b1cbf497f1e0c9486f54
Michael D <michael.drosdowski@gmail.com>
2022-01-31 23:11:14 +0100

    Loading Map, 1st Draft

--- CDungeonMap.h
+++ CDungeonMap.h
@@ -3,9 +3,9 @@
 
 #define FELD_MAX_X 30
 #define FELD_MAX_Y 30
-#define FELD_MAX_Z 1
+#define FELD_MAX_Z 14
 
-
+class TiXmlElement;
 class CDungeonMap
 {
 public:
@@ -16,7 +16,15 @@ public:
 	CField* GetField(int, int, int);
 	CField* GetField(VEKTOR);
 
+
+
 private:
+	void ParseTile(TiXmlElement* rootNode, int etage);
+	void ParseTiles(TiXmlElement* rootNode, int etage);
+	void ParseMap(TiXmlElement* rootNode, int etage);
+	void ParseMaps(TiXmlElement* rootNode);
 	CField* m_pFeld[FELD_MAX_X][FELD_MAX_Y][FELD_MAX_Z];
+	int m_LevelWidth[FELD_MAX_Z];
+	int m_LevelHeight[FELD_MAX_Z];
 };
 
