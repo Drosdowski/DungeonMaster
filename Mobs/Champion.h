489645e218c00cb09f0081fc9d602ca5abfa4c97
Michael <michael.drosdowski@gmail.com>
2023-09-13 17:47:23 +0200

    New class Champion (tbc)

---
+++ Mobs/Champion.h
@@ -0,0 +1,21 @@
+#pragma once
+class CChampion
+{
+	enum GenderType { // no more than these!
+		male,
+		female
+	};
+
+	CString name;
+	CString subname;
+	int pictureId;
+	GenderType gender;
+	VITALS vitals;
+
+};
+
+// Syra: Novice Priest / Apprentice WIzard   38/35/43/45/42/40	=> 53/72/15		AADFACNAAAAP	DHCGCDCLCNCKCI	AAAAAAAAADBBACDD
+// Gando: Apprentice Ninja / Novica Wizard   39/45/47/33/48/43  => 39/63/26		AACHACHGAABK	DCCHCNCPCBDACL	AAAADACDAAAABCBC
+// Elija: Novice Fighter / Apprentice Priest 42/40/42/36/53/40					AADMACEEAABG	DCCKCICKCEDFCI	BBCAAAAACBECAAAA
+// Hissa: Apprentice Fighter / Novice Ninja  58/48/35/35/43/55  => 80/61/5		AAFAACGCAAAF	CIDKDACDCDCLDH	EDAAADBAAAAAAAAA
+// Halk: Journeyman Figher					 55/43/30/46/38/48  => 90/75/0		AAFKACOOAAAA	CIDHCLBOCOCGDA	EAEAAAAAAAAAAAAA
